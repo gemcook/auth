f3b89b9f16d094b1146711c89d0ae9efd33ad845
gctfuji <t.fujimoto@gemcook.com>
2018-05-14 00:33:22 +0900

    [fix] ### Fix default logging

--- src/checkSession.js
+++ src/checkSession.js
@@ -1,14 +1,12 @@
 /* @flow */
 import {Auth} from 'aws-amplify';
 
-export default function checkSession(logging: boolean): Promise<*> {
-  const doLogging = !!logging;
-
+export default function checkSession(logging?: boolean = false): Promise<*> {
   return new Promise(async (resolve, reject) => {
     try {
       const session = await Auth.currentSession();
 
-      if (doLogging) {
+      if (logging) {
         console.warn(session);
       }
 
