31b4d958dfccd17113fbf06f9f41e7dffd12cd75
Takuya Fujimoto <t.fujimoto@gemcook.com>
2018-06-26 19:09:44 +0900

    [fix] ### Fix session

--- src/checkSession.js
+++ src/checkSession.js
@@ -5,6 +5,7 @@ export default function checkSession(logging?: boolean = false): Promise<*> {
   return new Promise(async (resolve, reject) => {
     try {
       const session = await Auth.currentSession();
+
       if (logging) {
         console.warn(Auth);
         console.warn(session);
